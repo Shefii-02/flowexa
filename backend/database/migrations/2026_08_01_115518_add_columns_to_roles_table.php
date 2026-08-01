dce74c8933f0e836389b3931399950547ae289f0
Shefii-02 <shefii.km@gmail.com>
2026-08-02 02:25:31 +0530

    fix

--- backend/database/migrations/2026_08_01_115518_add_columns_to_roles_table.php
+++ backend/database/migrations/2026_08_01_115518_add_columns_to_roles_table.php
@@ -10,19 +10,19 @@ return new class extends Migration
     {
         Schema::table('roles', function (Blueprint $table) {
             // Drop the existing unique index on name
-            // $table->dropUnique(['name']);
-            // $table->dropIndex(['name']);
-            // $table->dropColumn(['name']);
+            $table->dropUnique(['name']);
+            $table->dropIndex(['name']);
+            $table->dropColumn(['name']);
 
-            // $table->string('name', 50)->unique();
-            // // Add company_id
-            // $table->foreignId('company_id')
-            //     ->after('id')->nullable()
-            //     ->constrained('companies')
-            //     ->cascadeOnDelete();
+            $table->string('name', 50)->unique();
+            // Add company_id
+            $table->foreignId('company_id')
+                ->after('id')->nullable()
+                ->constrained('companies')
+                ->cascadeOnDelete();
 
-            // // Create composite unique
-            // $table->unique(['company_id', 'name']);
+            // Create composite unique
+            $table->unique(['company_id', 'name']);
         });
     }
 
